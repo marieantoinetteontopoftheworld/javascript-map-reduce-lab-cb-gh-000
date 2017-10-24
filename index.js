eceadde67a7e7ee4c535cfb71c1dbfd8cafb0542
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-24 20:50:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9027,7 +9027,7 @@ const openIssues = issues
 //console.log(openIssues);
 
 const nonAutomaticIssues = issues.reduce((nonAutomaticIssues, issue) => {
-  if (issue.body.includes('automatically created by learn.co')) {
+  if (!issue.body.includes('automatically created by learn.co')) {
     nonAutomaticIssues.push(issue);
   }
 
